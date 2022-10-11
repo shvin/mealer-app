6b8ac7d29f5f20c28bc67e8158849d916469a3e0
PatrickMeyer1 <meyerpatrick6378@gmail.com>
2022-10-10 23:27:26 -0400

    adding correct files

---
+++ app/src/main/java/com/example/seg2105_project/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.seg2105_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.seg2105_project.Client_Registration;
+import com.example.seg2105_project.R;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnClient;
+    Button btnCook;
+    Button btnAdmin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnClient = (Button) findViewById(R.id.btnClient);
+        btnCook = (Button) findViewById(R.id.btnCook);
+        btnAdmin = (Button) findViewById(R.id.btnAdmin);
+
+        btnClient.setOnClickListener(this);
+        btnCook.setOnClickListener(this);
+        btnAdmin.setOnClickListener(this);
+
+    }
+    public void onClick(View v){
+        switch(v.getId()) {
+            case R.id.btnClient:
+                startActivity(new Intent(this, Client_Registration.class));
+            case R.id.btnCook:
+                startActivity(new Intent(this, Cook_Registration.class));
+            case R.id.btnAdmin:
+                startActivity(new Intent(this, Admin_Login.class));
+        }
+    }
+}
